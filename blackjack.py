03b4a472bc5664923f95197dd396f92fed92dd4b
MissLexterPhD <alexyun04@gmail.com>
2019-05-09 13:21:44 -0700

    initial commit

---
+++ blackjack.py
@@ -0,0 +1,156 @@
+﻿import time
+import random
+
+card_chars=['┌','─', '┐','│','░','└','┘','♠', '♥', '♣', '♦']
+suits = ['♠', '♥', '♣', '♦']
+card_rank = ["A",2,3,4,5,6,7,8,9,10,"J","Q","K"]
+
+deck = list(range(52*4))
+cardheight = 9
+cardcount = 2
+cardwidth = 12
+
+# clubs deck[0-12] diamonds[13-25] hearts[26-38], spades[39-51]
+
+
+cardpart = ["┌──────────┐", "│░░░░░░░░░░│", "└──────────┘"]
+
+
+def printCards(hand, yn):
+    # position of number and suit on card
+    numberheight = 2
+    suitheight = int((cardheight-1)/2)
+
+    for rowno in range(cardheight):
+        # prints cards in hand in same line
+        for counter in range(len(hand)):
+            # determines which card part to print
+            if rowno == 0:
+                print(cardpart[0], end="    ") 
+            
+            if rowno == cardheight - 1:
+                print(cardpart[2], end="    ")
+            # prints dealer first card upside down
+            if yn == True and counter == 0:
+                if cardheight-1> rowno >= 1:
+                    print(cardpart[1], end="    ")
+            else:
+                
+                # prints number
+                if rowno == numberheight:
+                    cardn = hand[counter]%52%13 + 1
+                    print("│░░" + str(card_rank[cardn - 1]), end="")
+                    if cardn == 10:
+                        print("░░░░░░│", end="    ")
+                    else:
+                        print("░░░░░░░│", end="    ")
+                    
+                # prints suit
+                
+                
+                if rowno == suitheight:
+                    print("│░░░░" + suits[hand[counter] % 52//13] + "░░░░░│", end="    ")
+                       
+                    
+                else:
+                    """if not suit part of card
+                    and not bottom of card or
+                    top of card or number part of card
+                    print middle part of card
+                    """
+                    if rowno != cardheight - 1 and rowno != 0 and rowno != cardwidth/4 - 1:
+                        print(cardpart[1], end="    ")
+                """
+                if rowno == numberheight:
+                    print(cardValue)
+                """
+        print("")
+
+
+def calcScore(playerHand):
+    playerPoints = 0
+    ace = False
+    for counter in range(len(playerHand)):
+        if playerHand[counter]%52%13 + 1 == 1:
+            ace = True
+        if playerHand[counter]%52%13 + 1 >= 11:
+            cardValue = 10
+        else:
+            cardValue = playerHand[counter]%52%13 + 1
+        playerPoints += cardValue
+        
+        """
+        print("cardValue is", cardValue)
+        print("other thing is", playerHand[counter]%52%13 + 1)
+        """
+    # changes ace to value of 1 or 11
+    if playerPoints <= 11 and ace is True:
+        playerPoints += 10
+    return playerPoints
+
+
+def hit(player):
+    if player == "player":
+        player_hand.append(deck.pop())
+        
+    else:
+        dealer_hand.append(deck.pop())
+        
+            
+random.shuffle(deck)
+playforever = 1
+while playforever:
+    dealer_hand = [deck.pop(), deck.pop()]
+    player_hand = [deck.pop(), deck.pop()]
+    playerpoints = 0
+    print("Dealer hand:")
+    printCards(dealer_hand, True)
+    print("Your hand:")
+    printCards(player_hand, False)
+    keepgoing= "1"
+    # hit player
+    while keepgoing == "1" and calcScore(player_hand) < 21:
+        print("Your Score:", calcScore(player_hand))
+        keepgoing = input("hit or stand? (h/s, or q to quit) ")
+        if keepgoing.lower() != "h" and keepgoing.lower() != "s" and keepgoing.lower() != "q":
+            print("Please enter a valid response")
+            continue
+        elif keepgoing.lower() == "q":
+            playforever = 0
+            break
+        elif keepgoing == "1":
+            hit("player")
+            print("Your hand:")
+            printCards(player_hand, False)
+        elif calcScore(player_hand) >= 21:
+            break
+    
+    if calcScore(player_hand) > 21:
+        print("oof you busted")
+    else:
+    # hit dealer
+    
+        while calcScore(dealer_hand) <= 17:
+            hit("dealer")
+            if calcScore(dealer_hand) == 21:
+                break
+        print("Dealer's hand:")
+        printCards(dealer_hand, False)
+
+        if calcScore(player_hand) <= 21 and calcScore(dealer_hand) > 21:
+            print("Dealer lost")
+        elif calcScore(player_hand) > calcScore(dealer_hand) and calcScore(player_hand) <= 21:
+            if calcScore(player_hand) == 21:
+                print("BLECKJECK BOI")
+            else:
+                print("you won!")
+        else:
+            print("YOU LOST")
+    time.sleep(3)
+    #  [ 7, 21], 8, 9
+    print("")
+
+
+
+
+
